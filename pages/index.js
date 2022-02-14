9a75898a0ff8b12cebdff273eeae76b8abfe601f
liao <liaorio@gmail.com>
2022-02-14 10:42:28 -0500

    Add customized cursor

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import Link from "next/link";
+import CustomCursor from '../components/Cursor'
+
 
 export async function getStaticProps() {
   const response = await axios.get('https://6209943b6df46f0017f4c551.mockapi.io/api/v1/products');
@@ -14,15 +16,16 @@ export async function getStaticProps() {
 export default function Home({ productsData }) {
   return (
     <div>
-        <ul>
-          {productsData.map(({ id, name }, index) => (
-            <li key={index}>
-              <Link href={`/products/${id}`}>
-                <a>{name}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <CustomCursor />
+      <ul>
+        {productsData.map(({ id, name }, index) => (
+          <li key={index}>
+            <Link href={`/products/${id}`}>
+              <a>{name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
