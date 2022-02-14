035c39b9cfd8470b37fc1201d0cd5007ffe03019
liao <liaorio@gmail.com>
2022-02-13 22:09:20 -0500

    run-time build

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Link from "next/link";
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const response = await axios.get('https://6209943b6df46f0017f4c551.mockapi.io/api/v1/products');
   const productsData = response.data;
   return {
